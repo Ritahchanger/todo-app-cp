18f5cb23605126004dc9e986f360819ae64f5e01
Dennis Peter <peterdennis573@gmail.com>
2024-04-15 08:45:20 +0300

    Updates

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,38 +1,52 @@
-import React from "react";
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
+import React, { useState } from "react";
 
 const TodoApp = () => {
+  const [todos, setTodos] = useState([
+    "Codding some application",
+    "Designing some application",
+    "Taking Tea",
+    "Doing some serious application",
+  ]);
 
-  c
+  const [input, setInput] = useState("");
 
+  const addTodo = (event) => {
+    event.preventDefault();
+
+    setTodos([...todos, input]);
+  };
 
   return (
     <div className="w-full h-screen flex justify-center items-center flex-col">
-        <p className="text-center" >TODO APP</p>
+      <p className="text-center">TODO APP</p>
       <div className="container w-[500px] shadow-lg  flex justify-center items-center  p-2 flex-col">
-        <form action="#"> 
+        <form onSubmit={addTodo} className="flex justify-between items-cente">
           <input
             type="text"
             name="input"
-            id=""
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
             placeholder="Add todo item...."
             className="mr-2 outline-none border-b border-gray-500"
           />
 
           <button
             type="submit"
-            className="bg-blue-500 text-white py-1 px-4 rounded-sm "
+            disabled={!input}
+            className={`text-white py-1 cursor-pointer px-4 rounded-sm  ${
+              !input ? "bg-gray-500" : "bg-blue-600"
+            }`}
           >
             ADD
           </button>
         </form>
 
-        <ul >
-            <li>Codding some application</li>
-            <li>Designing some application</li>
-            <li>Taking Tea</li>
-            <li>Doing some serious application</li>
+        <ul className="flex flex-col items-start ">
+          {todos.map((todo) => (
+            <li>{todo}</li>
+          ))}
         </ul>
-
       </div>
     </div>
   );
